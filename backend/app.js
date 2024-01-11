aa51dfc88e09fdfde3e89ab16a64073fe6b7abf5
Laureen <laureen.pilo@gmail.com>
2024-01-11 15:02:01 +0100

    Ajout de middlewares dans le fichier app.js + amélioration du fichier server.js pour le rendre plus stable et approprié

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,28 @@ const express = require('express');
 
 const app = express();
 
+// 1er middleware qui enregistre "requête reçue" dans la console et passe l'exécution grâce à next
+app.use((req, res, next) => {
+    console.log('requête reçue');
+    next();
+});
+
+// 2ème middleware qui ajoute un code d'état 201 à la réponse et passe l'exécution
+app.use((req,res,next) => {
+    res.status(201);
+    next();
+})
 // méthode use avec une fonction qui reçoit la requête et la réponse 
 // et utilisation de l'objet réponse avec la méthode json pour renvoyer une réponse en json
-app.use((req, res) => {
+// 3ème middleware qui envoie la réponse JSON et passe l'exécution
+app.use((req, res, next) => {
     res.json({message: 'Votre requête a bien été reçue'});
+    next();
 });
+
+// dernier middleware qui enregistre "Réponse envoyée avec succès" dans la console
+app.use((req, res) => {
+    console.log('Réponse envoyée avec succès')
+})
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,52 @@ const http = require('http');
 // Importation du fichier app.js
 const app = require('./app');
 
-// indication du port sur lequel l'app va tourner grâce à la méthode set
-app.set('port', process.env.PORT || 3000);
-// création d'un serveur en appelant la méthode createServer avec passage de l'argument app dans la fonction server
-const server = http.createServer(app);
+//  la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un n° ou d'une chaîne
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+};
 
-// écoute/ attente des requêtes envoyées grâce à la méthode listen. On écoute le port 3000 par défaut,
-// mais s'il n'est pas disponible, utilisation de la variable environnement 'env' 
-server.listen(process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// La fonction errorHandler recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+};
+  
+const server = http.createServer(app);
+  
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+
+// Un écouteur d'événements est également enregistré, 
+// consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console
+server.listen(port);

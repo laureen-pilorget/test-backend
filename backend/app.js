9a86b9bebc704b65361ae54de5c41742fa8ebb3f
Laureen <laureen.pilo@gmail.com>
2024-01-21 10:51:01 +0100

    Création d'un fichier stuff.js pour y déplacer toutes les routes qui étaient dans le app.js + appel à ce nouveau fichier dans le fichier app

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,7 @@ const express = require('express');
 // intercepte toutes les requêtes qui ont un content-type JSON et nous mettent à disposition ce contenu sur l'objet requête dans req.body
 const app = express();
 const mongoose = require('mongoose');
-
-const Thing = require('./models/Thing');
+const stuffRoutes = require('./routes/stuff');
 
 // 
 mongoose.connect('mongodb+srv://laureenpilo:h6n8LSoYZDSaFlYW@cluster0.apxtpog.mongodb.net',
@@ -13,9 +12,9 @@ mongoose.connect('mongodb+srv://laureenpilo:h6n8LSoYZDSaFlYW@cluster0.apxtpog.mo
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  // Permet d'accéder au corps de la requête. Intercepte toutes les requêtes qui ont un content-type JSON 
+  // et nous met à disposition ce contenu sur l'objet requête dans req.body. L'ancienne version de ce middleware est le bodyParser
 app.use(express.json());
-
-
 
 // Création d'un middleware général (pas de route précisée) car appliqué à toutes les routes, toutes les requêtes envoyées à notre serveur
 app.use((req, res, next) => {
@@ -28,41 +27,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Pour intercepter uniquement les requêtes post (pour l'instant on log à la console parce qu'on a pas encore de DB). 
-// 201 est le code pour création de ressource
-app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
-// Création du middleware pour retourner un objet avec la méthode get
-app.get('/api/stuff/:id', (req, res, next) => {
-  // utilisation du modèle mongoose Thing avec la méthode findOne 
-  // auquel on passe l'objet de comparaison : on veut que l'ID de l'objet en vente soit le même que le paramètre de requête 
-  Thing.findOne({_id: req.params.id})
-    // un promise 
-    .then(thing => res.status(200).json(thing))
-    // une erreur 404 pour objet non trouvé
-    .catch(error => res.status(404).json({ error }));
-});
-
-
-// Création d'un middleware. 
-// On lui passe la méthode get avec les arguments req pour request, res pour response et next pour passer l'exécution
-// Mais aussi l'argument en string /api/stuff qui est la route pour laquelle on souhaite enregistrer cet élément middleware
-app.get('/api/stuff', (req, res, next) => {
-  // on lui demande renvoyer un tableau contenznt toutes les Things de la DB 
-  Thing.find()
-    // Et de nous retourner soit les éléments créés en json, soit une erreur 400
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
+// Ici on remet le début de la route : '/api/stuff' et on dit que pour cette route là, on utilise le router stuffRoutes
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;

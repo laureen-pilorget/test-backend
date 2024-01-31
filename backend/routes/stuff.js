7a2110ed69cc966e8d2c8f901dd705b725b04645
Laureen <laureen.pilo@gmail.com>
2024-01-31 17:09:39 +0100

    création du dossier middleware avec un fichier auth.js dedans qui vérifie que l'authentification est valide et donc que certaines routes sont accessibles uniquement via l'authentification

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const auth = require('auth');
 const router = express.Router();
+
+
+
 const stuffCtrl = require('../controllers/stuff');
 
-
-
-router.post('/', stuffCtrl.createThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.get('/', stuffCtrl.getAllThings);
-router.delete('/:id', stuffCtrl.deleteThing);
+// ajout du middleware auth pour vérifier la validité du token envoyé par le client et donc 
+// laisser accès à ces routes uniquement si le token est valide 
+// et permettre aux différentes routes d'en exploiter les différentes informations telles que le userId
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.get('/', auth, stuffCtrl.getAllThings);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
